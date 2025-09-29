2811012d83094b26f6457b091cf9f875b4668057
giogio555 <jordyhutahaean555@gmail.com>
2025-09-29 16:11:32 +0700

    gas

---
+++ js/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Set views directory path
+app.set('views', path.join(__dirname, '..', 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('landing_page');
+});
+
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
